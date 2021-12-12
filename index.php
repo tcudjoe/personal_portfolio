bf9eb217b6978bf9b5a4383c79a1b4d8d6313ec5
tyra cudjoe <tyra20015@gmail.com>
2021-12-12 16:08:27 +0100

    added form to contact section

--- index.php
+++ index.php
@@ -122,13 +122,23 @@
             <section id="contact-section">
                 <div class="container">
                     <div class="row">
-
+                        <div class="col-6">
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleFormControlInput1"
+                                    placeholder="name@example.com">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
         </div>
 
-        <footer>
+        <!-- <footer>
             <div class="container">
                 <div class="row justify-content-center">
                     <ul>
@@ -147,7 +157,7 @@
                     </ul>
                 </div>
             </div>
-        </footer>
+        </footer> -->
     </main>
 
 
